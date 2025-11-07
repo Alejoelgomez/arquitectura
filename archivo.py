89a305a4b7510b758d599d670a2524c35499be15
Alejoelgomez <diegoal.gomezsi@ecci.edu.co>
2025-11-07 10:52:54 -0500

    Add files via upload

---
+++ archivo.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+data={
+
+    'GENERO':['M','F','F','M','F','F'],
+    'EDAD':[12, 23, 55, 33, 15, 12],
+    'CARRERA':['ing', 'med', 'ing','mat','arqui','med']
+    
+                
+}
